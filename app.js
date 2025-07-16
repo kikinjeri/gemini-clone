6d6f1c91ad22e934b38cdb64bc47f03d62e762d5
Kiki <182134344+kikinjeri@users.noreply.github.com>
2025-07-16 18:58:30 -0400

    Add files via upload
    updated js
    import GoogleGenertiveAI r4om google/genertive-ai
    messages variable
    
    async function and await
    currently works in devtools
    
    in html: script type="importmap"

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
-function sendMessage() {
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const API_KEY = "AIzaSyBZKio1_RHXcKsCIM8Wnrlr6F8G3IPXNVs";
+const genAI = new GoogleGenerativeAI(API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+let messages =   {
+    history: [],
+            }
+
+
+async function sendMessage() {
     const userMessage = document.querySelector(".chat-window input").value;
-    alert(userMessage);
+
+    if (userMessage.length){
+        document.querySelector(".chat-window input").value = "";
+        document.querySelector(".chat-window .chat").insertAdjacentHTML(
+            "beforeend",
+            `<div class="user">
+            <p>${userMessage}</p>
+            </div>
+            `);
+    
+    const chat = model.startChat(messages);
+        
+          let result = await chat.sendMessage(userMessage);
+            console.log(result.response.text());
+
+           
+        
+    }
 }
+
 
 document.querySelector(".chat-window .input-area button")
 .addEventListener("click", ()=>sendMessage());
